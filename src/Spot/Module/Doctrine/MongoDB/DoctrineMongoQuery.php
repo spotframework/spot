59b63c228fca0ae437c3423e4019490c2e30c06e
William <tamanmini@gmail.com>
2013-06-26 15:06:55 +0700

    add countable interface for mongo query

--- src/Spot/Module/Doctrine/MongoDB/DoctrineMongoQuery.php
+++ src/Spot/Module/Doctrine/MongoDB/DoctrineMongoQuery.php
@@ -1,10 +1,15 @@
 <?php
 namespace Spot\Module\Doctrine\MongoDB;
 
+use Countable;
 use IteratorAggregate;
 use Doctrine\ODM\MongoDB\Query\Builder;
 
-class DoctrineMongoQuery extends Builder implements IteratorAggregate {
+class DoctrineMongoQuery extends Builder implements Countable, IteratorAggregate {
+    public function count() {
+        return $this->getIterator()->count();
+    }
+    
     public function getIterator() {
         return $this->getQuery()->execute();
     }    
