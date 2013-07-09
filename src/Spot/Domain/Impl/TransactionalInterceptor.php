901cd1e13fe864daa89587b8cce1cb78b4f1a0c4
William <tamanmini@gmail.com>
2013-07-09 16:53:53 +0700

    rename var

--- src/Spot/Domain/Impl/TransactionalInterceptor.php
+++ src/Spot/Domain/Impl/TransactionalInterceptor.php
@@ -8,27 +8,25 @@ use Spot\Aspect\Intercept\MethodInterceptor;
 
 class TransactionalInterceptor implements MethodInterceptor {
     private $domain,
-            $level = 0;
+            $lock = 0;
     
     public function __construct(DomainManager $domain) {
         $this->domain = $domain;
     }
     
     public function intercept(MethodInvocation $invocation) {
-        
-        
         try {
-            $this->level or $this->domain->begin();
+            $this->lock or $this->domain->begin();
             
-            ++$this->level;
+            ++$this->lock;
             
             $result = $invocation->proceed();
             
-            --$this->level or $this->domain->commit();
+            --$this->lock or $this->domain->commit();
             
             return $result;
         } catch(\RuntimeException $e) {
-            --$this->level or $this->domain->rollback();
+            --$this->lock or $this->domain->rollback();
             
             throw $e;
         }
