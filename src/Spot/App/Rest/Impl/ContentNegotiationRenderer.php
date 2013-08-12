31d2da9e8db5db7da85ab1fd71ff96562d1848ec
William <tamanmini@gmail.com>
2013-08-12 17:30:38 +0700

    rest app

--- src/Spot/App/Rest/Impl/ContentNegotiationRenderer.php
+++ src/Spot/App/Rest/Impl/ContentNegotiationRenderer.php
@@ -7,15 +7,10 @@ use Spot\App\Web\View;
 use Spot\App\Web\ViewRenderer;
 use Spot\App\Web\Impl\ScalarView;
 
-class ContentNegotiationRenderer implements ViewRenderer {
-    private $request;
-    
-    public function __construct(Request $request) {
-        $this->request = $request;
-    }
-    
-    public function render(View $view, Response $response) {
-        //TODO:implements
+class ContentNegotiationRenderer implements ViewRenderer {    
+    public function render(View $view, Request $request, Response $response) {
+        $response->setContentType("application/json");
+        echo json_encode($view->getModel()), "\n";
     }
 
     static public function rendererOf(View $view) {
