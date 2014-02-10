8ddd5b03429ccc0d48e72cd711f32be4af45222d
William <tamanmini@gmail.com>
2014-02-10 15:27:21 +0700

    monolog module

---
+++ src/Spot/Module/Monolog/MonologModule.php
@@ -0,0 +1,17 @@
+<?php
+namespace Spot\Module\Monolog;
+
+use Monolog\Logger;
+use Spot\Inject\Named;
+use Spot\Inject\Provides;
+use Spot\Inject\Singleton;
+
+class MonologModule {
+    /** @Provides("Psr\Log\LoggerInterface") @Singleton */
+    static public function provideLogger(
+            /** @Named("monolog.name") */$name,
+            /** @Named("monolog.handlers") */array $handlers = [],
+            /** @Named("monolog.processors") */array $processors = []) {
+        return new Logger($name, $handlers, $processors);
+    }
+}
