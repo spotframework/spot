e683b3575aed90919dc48c0ccc129a3f5c079145
William <tamanmini@gmail.com>
2014-02-11 16:24:34 +0700

    empty default monolog name

--- src/Spot/Module/Monolog/MonologModule.php
+++ src/Spot/Module/Monolog/MonologModule.php
@@ -9,7 +9,7 @@ use Spot\Inject\Singleton;
 class MonologModule {
     /** @Provides("Psr\Log\LoggerInterface") @Singleton */
     static public function provideLogger(
-            /** @Named("monolog.name") */$name,
+            /** @Named("monolog.name") */$name = "",
             /** @Named("monolog.handlers") */array $handlers = [],
             /** @Named("monolog.processors") */array $processors = []) {
         return new Logger($name, $handlers, $processors);
