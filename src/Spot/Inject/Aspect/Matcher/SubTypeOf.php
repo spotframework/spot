daadd1e5c98dc2c915d54de56d163b687466c344
William <tamanmini@gmail.com>
2014-02-12 16:29:54 +0700

    add @Annotation

--- src/Spot/Inject/Aspect/Matcher/SubTypeOf.php
+++ src/Spot/Inject/Aspect/Matcher/SubTypeOf.php
@@ -4,6 +4,7 @@ namespace Spot\Inject\Aspect\Matcher;
 use Spot\Inject\Aspect\MethodMatcher;
 use Spot\Reflect\Method;
 
+/** @Annotation */
 class SubTypeOf implements MethodMatcher {
     public $value;
 
