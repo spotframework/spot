52951bcc980f0c7ba43f8ca3e7e65a893810894a
William <tamanmini@gmail.com>
2014-02-12 14:33:00 +0700

    subtype AOP matcher

---
+++ src/Spot/Inject/Aspect/Matcher/SubTypeOf.php
@@ -0,0 +1,13 @@
+<?php
+namespace Spot\Inject\Aspect\Matcher;
+
+use Spot\Inject\Aspect\MethodMatcher;
+use Spot\Reflect\Method;
+
+class SubTypeOf implements MethodMatcher {
+    public $value;
+
+    function matches(Method $method) {
+        return $method->getType()->isSubtypeOf($this->value);
+    }
+}
