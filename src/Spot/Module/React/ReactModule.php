9e97c357798bc61b659e47a4fee11087a22c1155
William <tamanmini@gmail.com>
2014-02-28 21:16:16 +0700

    react module

---
+++ src/Spot/Module/React/ReactModule.php
@@ -0,0 +1,15 @@
+<?php
+namespace Spot\Module\React;
+
+use Spot\Inject\Provides;
+use Spot\Inject\Named;
+use Spot\Inject\Singleton;
+use React\EventLoop\LoopInterface;
+use React\EventLoop\Factory;
+
+class ReactModule {
+    /** @Provides("\React\EventLoop\LoopInterface") @Singleton */
+    static public function provideEventLoop() {
+        return Factory::create();
+    }
+}
