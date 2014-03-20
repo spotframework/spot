40b3578158c9e2cdc7d55a9196255f3ceac81218
William <tamanmini@gmail.com>
2014-03-20 16:35:00 +0700

    add default port, username and password for swiftmailer smtp module

--- src/Spot/Module/SwiftMailer/SwiftSmtpModule.php
+++ src/Spot/Module/SwiftMailer/SwiftSmtpModule.php
@@ -8,8 +8,14 @@ class SwiftSmtpModule {
     /** @Provides("Swift_Transport") */
     static public function provideSmtpTransport(
             /** @Named("swiftmailer.smtp.host") */$host,
-            /** @Named("swiftmailer.smtp.port") */$port,
+            /** @Named("swiftmailer.smtp.port") */$port = 465,
+            /** @Named("swiftmailer.smtp.username") */$username = null,
+            /** @Named("swiftmailer.smtp.password") */$password = null,
             /** @Named("swiftmailer.smtp.security") */$security = "tls") {
-        return \Swift_SmtpTransport::newInstance($host, $port, $security);
+        $smtp = \Swift_SmtpTransport::newInstance($host, $port, $security);
+        $username && $smtp->setUsername($username);
+        $password && $smtp->setPassword($password);
+        
+        return $smtp;
     }
 }
